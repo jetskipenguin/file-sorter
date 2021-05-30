edbf1c15b23b32da483ff84c4b64e7255506c914
Collin <collinnewman12@gmail.com>
2021-05-29 23:39:18 -0500

    added folder class

--- sorter.py
+++ sorter.py
@@ -1,134 +1,76 @@
+'''
+KNOWN ERROR: Don't rename a duplicate to another duplicate in the same file, the program won't detect it and there will be an error
+'''
 import shutil
 import os
 
 source = os.getcwd()
 source_files = [file for file in os.listdir(source)]
 
-# sorts file endings into folders
-img = ['png', 'jpg', 'gif', 'jpeg', 'psd', 'tif']
-img_folder = source + "\\Image Files"
+class folder:
+    # creates class attributes
+    def __init__(self, files, name):
+        self.count = 0
+        self.files = files
+        self.name = name
+        self.path = source + "\\" + name
 
-compressed = ['zip', 'rar', '7z']
-compressed_folder = source + "\\Compressed Files"
+    # creates folder if it does not exist
+    def check_folder(self):
+        if(self.path.replace(source, '').strip('\\') not in source_files):
+            os.mkdir(self.name)
+            print("Created {} Folder".format(self.name))
 
-exe = ['exe', 'jar', 'msi']
-exe_folder = source + "\\Exe Files"
+    # returns duplicates
+    def check_duplicates(self):
+        file_list = [file for file in os.listdir(self.path)]
+        duplicate_list = []
 
-pdf = ['pdf']
-pdf_folder = source + "\\PDF Files"
+        # finds files in source with same name as those in folder
+        for x in os.listdir(source):
+            for y in file_list:
+                if x == y:
+                    duplicate_list.append(x)
 
-document = ['txt', 'docx', 'rtf', 'xlsx', 'ppt', 'log']
-document_folder = source + "\\Documents"
+        return duplicate_list
+    
+    def move_files(self):
+        for file in source_files:
+            for i in self.files:
+                if(file.endswith(i)):
+                    shutil.move(file, self.path)
+                    self.count += 1
 
-media = ['mp4', 'mp3']
-media_folder = source + "\\Media Files"
+folders = []
 
+# adds folder objects to folders list
+folders.append(folder(['png', 'jpg', 'gif', 'jpeg', 'psd', 'tif'], "Image Files"))
+folders.append(folder(['zip', 'rar', '7z'], "Compressed Files"))
+folders.append(folder(['exe', 'jar', 'msi'], "Exe Files"))
+folders.append(folder(['pdf'], "PDF Files"))
+folders.append(folder(['txt', 'docx', 'rtf', 'xlsx', 'ppt', 'log'], "Documents"))
+folders.append(folder(['mp4', 'mp3'], "Media Files"))
 
-# returns a list of duplicate files
-def get_duplicates(path):
-    file_list2 = [file for file in os.listdir(path)]
-    duplicate_list = []
+duplicates = []
 
-    for x in source_files:
-        for y in file_list2:
-            if x == y:
-                duplicate_list.append(x)
+# creates necessary folders and finds files with duplicate names
+for folder in folders:
+    folder.check_folder()
+    folder.check_duplicates()
+    duplicates.append(folder.check_duplicates())
 
-    return duplicate_list
+# prompts user input to rename dupes
+count = 0
+for file in duplicates:
+    # makes sure list isn't empty
+    if file:
+        new_name = input('Rename {} to a name not already taken (include file extension):\n'.format(file[count]))
+        os.rename(source + '\\' + file[count], source + '\\' + new_name)
 
-# allows for renaming of files in duplicate_list
-def rename_duplicates(list1):
-    for i in list1:
-        new_name = input('Rename {} to a name not already taken (include file extension):\n'.format(i))
-        os.rename(source + '\\' + i, source + '\\' + new_name)
+# move files to appropriate folder
+for folder in folders:
+    folder.move_files()
 
-# creates necessary folders
-if(img_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(img_folder)
-    print("Created Image Folder")
-
-if(compressed_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(compressed_folder)
-    print("Created Compressed Files Folder")
-
-if(exe_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(exe_folder)
-    print("Created Exe Folder")
-
-if(pdf_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(pdf_folder)
-    print("Created PDF Folder")
-
-if(document_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(document_folder)
-    print("Created Documents Folder")
-
-if(media_folder.replace(source, '').strip('\\') not in source_files):
-    os.mkdir(media_folder)
-    print("Created Media Folder")
-
-
-# checks for duplicates in each folder and prompts the user to rename
-img_dupes = get_duplicates(img_folder)
-rename_duplicates(img_dupes)
-
-compressed_dupes = get_duplicates(compressed_folder)
-rename_duplicates(compressed_dupes)
-
-exe_dupes = get_duplicates(exe_folder)
-rename_duplicates(exe_dupes)
-
-pdf_dupes = get_duplicates(pdf_folder)
-rename_duplicates(pdf_dupes)
-
-document_dupes = get_duplicates(document_folder)
-rename_duplicates(document_dupes)
-
-media_dupes = get_duplicates(media_folder)
-rename_duplicates(media_dupes)
-
-img_count = 0
-compressed_count = 0
-exe_count = 0
-pdf_count = 0
-document_count = 0
-media_count = 0
-
-# moves files to correct folder based on file ending
-for file in os.listdir(source):
-    for i in img:
-        if file.endswith(i):
-            shutil.move(file, img_folder)
-            img_count += 1
-
-    for i in compressed:
-        if file.endswith(i):
-            shutil.move(file, compressed_folder)
-            compressed_count += 1
-
-    for i in exe:
-        if file.endswith(i):
-            shutil.move(file, exe_folder)
-            exe_count += 1
-
-    for i in pdf:
-        if file.endswith(i):
-            shutil.move(file, pdf_folder)
-            pdf_count += 1
-
-    for i in document:
-        if file.endswith(i):
-            shutil.move(file, document_folder)
-            document_count += 1
-
-    for i in media:
-        if file.endswith(i):
-            shutil.move(file, media_folder)
-            media_count += 1
-
-print('Successfully moved {} image files'.format(img_count))
-print('Successfully moved {} compressed files'.format(compressed_count))
-print('Successfully moved {} exe files'.format(exe_count))
-print('Successfully moved {} pdf files'.format(pdf_count))
-print('Successfully moved {} doc files'.format(document_count))
-print('Successfully moved {} media files'.format(media_count))
+# print number of files moved
+for folder in folders:
+    print("Successfully moved {} files to ".format(folder.count) + folder.name + " folder")
