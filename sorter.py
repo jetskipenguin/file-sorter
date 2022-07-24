3c2ccadb16a793cd03998e4efdd7633b8c19aef2
Collin Newman <45407009+jetskipenguin@users.noreply.github.com>
2022-07-24 15:14:37 -0500

    Auto rename duplicate files

--- sorter.py
+++ sorter.py
@@ -1,23 +1,19 @@
-'''
-KNOWN ERROR: Don't rename a duplicate to another duplicate in the same file, the program won't detect it and there will be an error
-'''
 import shutil
 import os
 
 source = os.getcwd()
-source_files = [file for file in os.listdir(source)]
 
 class folder:
     # creates class attributes
     def __init__(self, files, name):
         self.count = 0
-        self.files = files
+        self.files = [file for file in os.listdir(source)]
         self.name = name
         self.path = source + "\\" + name
 
     # creates folder if it does not exist
     def check_folder(self):
-        if(self.path.replace(source, '').strip('\\') not in source_files):
+        if(self.path.replace(source, '').strip('\\') not in self.files):
             os.mkdir(self.name)
             print("Created {} Folder".format(self.name))
 
@@ -32,10 +28,11 @@ class folder:
                 if x == y:
                     duplicate_list.append(x)
 
+        # flatten list and return
         return duplicate_list
     
     def move_files(self):
-        for file in source_files:
+        for file in os.listdir(source):
             for i in self.files:
                 if(file.endswith(i)):
                     shutil.move(file, self.path)
@@ -44,11 +41,11 @@ class folder:
 folders = []
 
 # adds folder objects to folders list
-folders.append(folder(['png', 'jpg', 'gif', 'jpeg', 'psd', 'tif'], "Image Files"))
+folders.append(folder(['png', 'jpg', 'gif', 'jpeg', 'psd', 'tif', 'jfif'], "Image Files"))
 folders.append(folder(['zip', 'rar', '7z'], "Compressed Files"))
 folders.append(folder(['exe', 'jar', 'msi'], "Exe Files"))
 folders.append(folder(['pdf'], "PDF Files"))
-folders.append(folder(['txt', 'docx', 'rtf', 'xlsx', 'ppt', 'log'], "Documents"))
+folders.append(folder(['txt', 'docx', 'rtf', 'xlsx', 'ppt', 'log', 'pptx'], "Documents"))
 folders.append(folder(['mp4', 'mp3'], "Media Files"))
 
 duplicates = []
@@ -56,16 +53,21 @@ duplicates = []
 # creates necessary folders and finds files with duplicate names
 for folder in folders:
     folder.check_folder()
-    folder.check_duplicates()
     duplicates.append(folder.check_duplicates())
 
 # prompts user input to rename dupes
+print(duplicates)
 count = 0
 for file in duplicates:
-    # makes sure list isn't empty
     if file:
-        new_name = input('Rename {} to a name not already taken (include file extension):\n'.format(file[count]))
-        os.rename(source + '\\' + file[count], source + '\\' + new_name)
+        name = file[count].split('.')[0]
+        extension = file[count].split('.')[1]
+        for num in range(1, 10000):
+            new_name = name + f'({num}).' + extension
+            if new_name not in os.listdir(source):
+                os.rename(source + '\\' + file[count], source + '\\' + new_name)
+                break
+        count += 1
 
 # move files to appropriate folder
 for folder in folders:
